b22b41243defa5616027bc0421c4f0cc95db3999
necip <ncpyolcu@gmail.com>
2024-07-12 10:44:59 +0200

    deploy-3

--- api/user/userRoutes.ts
+++ api/user/userRoutes.ts
@@ -3,11 +3,11 @@ import express, { Router } from 'express';
 import { generateCode, validateCode, createUser, loginUser, getUsers } from './userController';
 const router: Router = express.Router();
 
-router.post('/generate-invitation-code', generateCode);
-router.post('/validate-invitation-code', validateCode);
+// router.post('/generate-invitation-code', generateCode);
+// router.post('/validate-invitation-code', validateCode);
 
-router.post('/register', createUser);
-router.post('/login', loginUser);
-router.get('/listUsers',getUsers);
+// router.post('/register', createUser);
+// router.post('/login', loginUser);
+// router.get('/listUsers',getUsers);
 
 export default router;
