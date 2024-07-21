71b8eb79d92ae74c7ac2d0c84bac944fec825296
necip <ncpyolcu@gmail.com>
2024-07-21 22:37:17 +0200

    mongodb_control

--- api/index.ts
+++ api/index.ts
@@ -2,13 +2,21 @@ import express, { Request, Response } from "express";
 
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import userRoutes from './user/userRoutes';
 import itemRoutes from './item/itemRoutes';
+import mongoose from "mongoose";
+
+dotenv.config(); // Load environment variables from .env file
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+
+mongoose.connect(process.env.DATABASE_URL as string)
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 
 ///////////////////////////
@@ -22,18 +30,6 @@ const users = [
 app.get('/', (req: Request, res: Response) => {
   res.json(users);
 });
-
-/* // GET /user/:id
-app.get('/:id', (req: Request, res: Response) => {
-  const user = users.find(u => u.id === parseInt(req.params.id));
-  if (user) {
-    res.json(user);
-  } else {
-    res.status(404).json({ message: 'User not found' });
-  }
-});
-///////////////////////////*/
-
 
 // Use routes
 app.use('/user', userRoutes);
