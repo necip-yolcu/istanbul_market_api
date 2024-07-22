2bfad810f80ab7b7a37e2686b9d7de518dd18893
necip <ncpyolcu@gmail.com>
2024-07-22 02:19:46 +0200

    usre role is added

--- api/user/userController.ts
+++ api/user/userController.ts
@@ -61,6 +61,7 @@ const loginUser = async (req: AuthRequest, res: Response): Promise<void> => {
       id: user.id,
       name: user.name,
       email: user.email,
+      role: user.role,
       token,
     });
     return;
